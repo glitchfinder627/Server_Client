8590cfda7ae96f2223b2b59e9a0f91fa17ea2589
glitchfinder627 <rupamdebnath627@gamil.com>
2019-02-14 10:15:44 +0530

    Port Set

--- Server.py
+++ Server.py
@@ -1,6 +1,7 @@
 import time
 import threading
 import socketserver
+import os
 
 class TCPRequestHandler(socketserver.BaseRequestHandler):
 	def handle(self):
@@ -23,19 +24,28 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
 if __name__ == "__main__":
+	
+	ON_HEROKU = os.environ.get('ON_HEROKU')
 
-    HOST, PORT = "localhost", 8745
+	port = None
+	if ON_HEROKU:
+	    # get the heroku port
+	    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+	else:
+	    port = 8745	
+	
+	print(port)	
 
-    with ThreadedTCPServer((HOST, PORT), TCPRequestHandler) as server:
+	HOST, PORT = "localhost", port
 
-        print("Server address - {}:{}".format(server.server_address[0],str(server.server_address[1])))
+	server = ThreadedTCPServer((HOST, PORT), TCPRequestHandler)
+	print("Server address - {}:{}".format(server.server_address[0],str(server.server_address[1])))
+	server_thread = threading.Thread(target=server.serve_forever)
+	server_thread.daemon = True
+	server_thread.start()
+	print("Server loop running in thread:", server_thread.name)
 
-        server_thread = threading.Thread(target=server.serve_forever)
-        server_thread.daemon = True
-        server_thread.start()
-        print("Server loop running in thread:", server_thread.name)
+	while True:
+		time.sleep(50)
 
-        while True:
-            time.sleep(50)
-
-        server.shutdown()
+	server.shutdown()
